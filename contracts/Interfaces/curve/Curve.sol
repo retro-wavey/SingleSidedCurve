3288dc8befbc45cee1d64d02fae7511452cf9699
orbxball <orbxball@protonmail.com>
2021-04-22 12:19:55 -0400

    new curve interface for metapool

--- contracts/Interfaces/curve/Curve.sol
+++ contracts/Interfaces/curve/Curve.sol
@@ -44,6 +44,13 @@ interface ICurveFi {
         bool use_underlying
     ) external payable;
 
+    // Curve.fi Factory USD Metapool v2
+    function add_liquidity(
+        address pool,
+        uint256[4] calldata amounts,
+        uint256 min_mint_amount
+    ) external;
+
     function coins(uint256) external returns (address);
     function underlying_coins(uint256) external returns (address);
 
