740c65a4cec1ebb163e18ac6d4ba89775980126f
Poolpi Tako <poolpitako@protonmail.com>
2021-05-26 15:19:25 -0500

    feat: linted the repo

--- contracts/Interfaces/curve/Curve.sol
+++ contracts/Interfaces/curve/Curve.sol
@@ -45,15 +45,27 @@ interface ICurveFi {
     ) external payable;
 
     function coins(uint256) external returns (address);
+
     function underlying_coins(uint256) external returns (address);
 
-    function remove_liquidity_imbalance(uint256[2] calldata amounts, uint256 max_burn_amount) external;
+    function remove_liquidity_imbalance(
+        uint256[2] calldata amounts,
+        uint256 max_burn_amount
+    ) external;
 
-    function remove_liquidity(uint256 _amount, uint256[2] calldata amounts) external;
+    function remove_liquidity(uint256 _amount, uint256[2] calldata amounts)
+        external;
 
-    function calc_withdraw_one_coin(uint256 _amount, int128 i) external view returns (uint256);
+    function calc_withdraw_one_coin(uint256 _amount, int128 i)
+        external
+        view
+        returns (uint256);
 
-    function calc_withdraw_one_coin(uint256 _amount, int128 i, bool use_underlying) external view returns (uint256);
+    function calc_withdraw_one_coin(
+        uint256 _amount,
+        int128 i,
+        bool use_underlying
+    ) external view returns (uint256);
 
     function remove_liquidity_one_coin(
         uint256 _token_amount,
@@ -83,9 +95,20 @@ interface ICurveFi {
         uint256 _from_amount
     ) external view returns (uint256);
 
-    function calc_token_amount( uint256[2] calldata amounts, bool is_deposit) external view returns (uint256);
-    function calc_token_amount( uint256[3] calldata amounts, bool is_deposit) external view returns (uint256);
-    function calc_token_amount( uint256[4] calldata amounts, bool is_deposit) external view returns (uint256);
+    function calc_token_amount(uint256[2] calldata amounts, bool is_deposit)
+        external
+        view
+        returns (uint256);
+
+    function calc_token_amount(uint256[3] calldata amounts, bool is_deposit)
+        external
+        view
+        returns (uint256);
+
+    function calc_token_amount(uint256[4] calldata amounts, bool is_deposit)
+        external
+        view
+        returns (uint256);
 }
 
 interface Zap {
--- tests/test_live.py
+++ tests/test_live.py
@@ -2,7 +2,7 @@ from itertools import count
 from brownie import Wei, reverts
 import eth_abi
 from brownie.convert import to_bytes
-from useful_methods import genericStateOfStrat,genericStateOfVault
+from useful_methods import genericStateOfStrat, genericStateOfVault
 import random
 import brownie
 
@@ -12,17 +12,20 @@ import brownie
 #           - change in loading (from low to high and high to low)
 #           - strategy operation at different loading levels (anticipated and "extreme")
 
-def test_opsss_lvie(currency,live_strategy, chain,live_vault, gov, samdev,strategist, interface):
+
+def test_opsss_lvie(
+    currency, live_strategy, chain, live_vault, gov, samdev, strategist, interface
+):
     strategy = live_strategy
     vault = live_vault
     strategist = samdev
     gov = samdev
 
-    #currency.approve(vault, 2 ** 256 - 1, {"from": whale} )
-    #whalebefore = currency.balanceOf(whale)
-   # whale_deposit  = 100 *1e18
-    #vault.deposit(whale_deposit, {"from": whale})
-    strategy.harvest({'from': strategist})
+    # currency.approve(vault, 2 ** 256 - 1, {"from": whale} )
+    # whalebefore = currency.balanceOf(whale)
+    # whale_deposit  = 100 *1e18
+    # vault.deposit(whale_deposit, {"from": whale})
+    strategy.harvest({"from": strategist})
 
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfVault(vault, currency)
@@ -30,38 +33,44 @@ def test_opsss_lvie(currency,live_strategy, chain,live_vault, gov, samdev,strate
     chain.sleep(2592000)
     chain.mine(1)
 
-    strategy.harvest({'from': strategist})
-    steth = interface.ERC20('0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84')
+    strategy.harvest({"from": strategist})
+    steth = interface.ERC20("0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84")
 
-    print("steth = ", steth.balanceOf(strategy)/1e18)
-    print("eth = ", strategy.balance()/1e18)
+    print("steth = ", steth.balanceOf(strategy) / 1e18)
+    print("eth = ", strategy.balance() / 1e18)
 
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfVault(vault, currency)
 
-    print("\nEstimated APR: ", "{:.2%}".format(((vault.totalAssets()-1000*1e18)*12)/(1000*1e18)))
+    print(
+        "\nEstimated APR: ",
+        "{:.2%}".format(((vault.totalAssets() - 1000 * 1e18) * 12) / (1000 * 1e18)),
+    )
 
-   # vault.withdraw({"from": whale})
+    # vault.withdraw({"from": whale})
     print("\nWithdraw")
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfVault(vault, currency)
-  # print("Whale profit: ", (currency.balanceOf(whale) - whalebefore)/1e18)
 
 
-def test_migrate_live(currency,Strategy, live_strategy,live_vault, chain, whale,samdev, interface):
+# print("Whale profit: ", (currency.balanceOf(whale) - whalebefore)/1e18)
+
+
+def test_migrate_live(
+    currency, Strategy, live_strategy, live_vault, chain, whale, samdev, interface
+):
     strategy = live_strategy
     vault = live_vault
     strategist = samdev
     gov = samdev
 
-    strategy.harvest({'from': strategist})
+    strategy.harvest({"from": strategist})
 
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfVault(vault, currency)
 
-
     strategy2 = strategist.deploy(Strategy, vault)
-    vault.migrateStrategy(strategy, strategy2, {'from': gov})
+    vault.migrateStrategy(strategy, strategy2, {"from": gov})
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfStrat(strategy2, currency, vault)
     genericStateOfVault(vault, currency)
